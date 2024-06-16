424ea2d9ef9c6ada81ebfabb71febcfcad8c2e1f
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-06-16 23:11:51 +0700

    add some grid view but there is some problem in overload

--- lib/pages/content/home_screen.dart
+++ lib/pages/content/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:pijar_app/widgets/big_card.dart';
+import 'package:pijar_app/widgets/small_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,72 +9,114 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(24),
-        child: SafeArea(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey.shade200,
-                    borderRadius: BorderRadius.circular(32),
-                  ),
-                  child: Row(
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.search),
-                        onPressed: () {
-                          // Implement the functionality for the left icon
-                        },
-                      ),
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: "Search by artist, place, or genre...",
-                            border: InputBorder.none,
-                            contentPadding: EdgeInsets.symmetric(vertical: 10),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    child: Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.search),
+                          onPressed: () {
+                            // Implement the functionality for the left icon
+                          },
+                        ),
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: "Search by artist, place, or genre...",
+                              border: InputBorder.none,
+                              contentPadding:
+                                  EdgeInsets.symmetric(vertical: 10),
+                            ),
                           ),
                         ),
+                        IconButton(
+                          icon: Icon(Icons.menu),
+                          onPressed: () {
+                            // Implement the functionality for the right icon
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                Row(
+                  children: [
+                    Text(
+                      "For you",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+                    ),
+                    Spacer(),
+                    Text(
+                      "See all",
+                      style:
+                          TextStyle(color: Colors.blue.shade400, fontSize: 16),
+                    )
+                  ],
+                ),
+                SizedBox(height: 16),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      BigCard(
+                          imagePath: "lib/assets/coldPlay.png",
+                          title: "Coldplay for Charity Concert",
+                          location: "Yogyakarta",
+                          date: "Aug 10"),
+                      SizedBox(
+                        width: 16,
                       ),
-                      IconButton(
-                        icon: Icon(Icons.menu),
-                        onPressed: () {
-                          // Implement the functionality for the right icon
-                        },
-                      ),
+                      BigCard(
+                          imagePath: "lib/assets/blues.png",
+                          title: "Blues for Betterment",
+                          location: "Jakarta",
+                          date: "June 16")
                     ],
                   ),
                 ),
-              ),
-              SizedBox(height: 16),
-              Row(
-                children: [
-                  Text(
-                    "For you",
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
-                  ),
-                  Spacer(),
-                  Text(
-                    "See all",
-                    style: TextStyle(color: Colors.blue.shade400, fontSize: 16),
-                  )
-                ],
-              ),
-              SizedBox(height: 16),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  
-                  children: [
-                    BigCard(imagePath: "lib/assets/coldPlay.png", title: "Coldplay for Charity Concert", location: "Yogyakarta", date: "Aug 10"),
-                    SizedBox(width: 16,),
-                    BigCard(imagePath: "lib/assets/blues.png", title: "Blues for Betterment", location: "Jakarta", date: "June 16")
-                  ],
+                SizedBox(
+                  height: 24,
                 ),
-              )
-            ],
+                Text(
+                  "Find a Concert",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                GridView.count(
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                  crossAxisCount: 2, // Menentukan jumlah kolom
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  children: List.generate(
+                    4, // Number of SmallCard widgets
+                    (index) => SmallCard(
+                      imagePath: "lib/assets/blues.png",
+                      title: "Don’t Drunk Henry",
+                      location: "Pekanbaru",
+                      date: "Aug 30",
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/widgets/small_card.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class SmallCard extends StatelessWidget {
+  final String imagePath;
+  final String title;
+  final String location;
+  final String date;
+
+  const SmallCard({
+    Key? key,
+    required this.imagePath,
+    required this.title,
+    required this.location,
+    required this.date,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3), // Warna bayangan
+            spreadRadius: 2, // Menyebar radius
+            blurRadius: 4, // Blur radius
+            offset: Offset(0, 2), // Offset dari bayangan (x, y)
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Column(
+          children: [
+            Image.asset(
+              imagePath,
+              width: double.infinity,
+              height: 150,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 8),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.map,
+                        color: Colors.grey,
+                        size: 16,
+                      ),
+                      SizedBox(width: 4),
+                      Text(
+                        location,
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      Spacer(),
+                      Text(
+                        date,
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
