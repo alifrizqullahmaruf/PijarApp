9976bc97f0ae201b82028b75f653dc2a0dcbdf98
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-06-19 08:25:11 +0700

    Add some content in notification screen

--- lib/pages/content/notification_screen.dart
+++ lib/pages/content/notification_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:pijar_app/widgets/search_bar.dart';
 
 class NotificationScreen extends StatelessWidget {
   const NotificationScreen({super.key});
@@ -7,15 +9,36 @@ class NotificationScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          Center(
-            child: Text(
-              "NotificationScreen",
-              style: TextStyle(fontSize: 36),
+          child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          children: [
+            SearchBarWidgets(),
+            Expanded(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment
+                      .center, // Menyelaraskan di tengah sumbu utama
+                  crossAxisAlignment: CrossAxisAlignment
+                      .center, // Menyelaraskan di tengah sumbu silang
+                  children: [
+                    Image.asset(
+                      "lib/assets/notification.png",
+                      height: 200,
+                    ),
+                    SizedBox(
+                        height: 16), // Menambahkan jarak antara gambar dan teks
+                    Text(
+                      "Nothing Here!!!",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 24),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          )
-        ],
+          ],
+        ),
       )),
     );
   }
